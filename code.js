53499ffc4a1b752b00dd1e1fdcdb69c3513a8872
Marlena <marlenaw@student.agh.edu.pl>
2025-04-08 16:29:37 +0200

    jakies cos chyba nic

--- code.js
+++ code.js
@@ -16,13 +16,22 @@ function weryfikacja()
     var pole_wiadomosc=form["wiadomosc"].value;
     if(!walidujNapis(pole_imie, "podaj imie!")) return false;
     if(!walidujNapis(pole_email, "podaj email!")) return false;
+    if (!poprawnyAdres(pole_email))
+        {
+            return false;
+        }
     if (!walidujNapis(pole_kod, "Podaj kod pocztowy!")) return false;
     if (!walidujNapis(pole_wiadomosc, "Wpisz wiadomość!")) return false;
-
-    return true;
-
-   
+    
+    return true;  
 }
+/*
+var formularz =document forms["user_data"]
+
+for (var i=0; i<formularz.length; i++)
+{
+walidujNapis(formularz[i].value, "Uzupełnij pole"+formularz[i].name)}
+*/
 function bialeZnaki(string)
 {
     return string.trim().length===0;
@@ -36,4 +45,31 @@ function walidujNapis(ciag, komunikat)
         return false;
     }
     return true;
+}
+/*
+
+function walidujNapis(ciag, komunikat)
+{
+if (PustoTu(ciag))
+{
+alert(komunikat)
+return false;
+
+}
+else
+{
+if(bialeZnaki(ciag))
+alert(komunikat)
+return false;}
+}
+return true;
+*/
+function poprawnyAdres(adres) {
+    let email_form = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/;
+    if (email_form.test(adres))
+        return true;
+    else {
+        alert("Adres e-mail jest niepoprawny");
+        return false;
+    }
 }
